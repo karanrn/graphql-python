1a2494ed86cabe9e7b8c45245cb758e06d8e7a90
Karan Nadagoudar <karan4080@gmail.com>
2021-03-17 10:06:32 +0530

    Search capabilties for query

--- app/api/schema.py
+++ app/api/schema.py
@@ -7,13 +7,27 @@ from users.modules import UserObject, AddUser
 
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
-    all_users = graphene.List(UserObject)
-    all_books = graphene.List(BookObject)
+    all_users = graphene.List(UserObject, search=graphene.String())
+    all_books = graphene.List(BookObject, search=graphene.String())
 
-    def resolve_all_users(self, info, **kwargs):
+    def resolve_all_users(self, info, search=None, **kwargs):
+        if search:
+            # Search both username and email
+            filter_user = (
+                User.username.like('%'+search+"%") | 
+                User.email.like('%'+search+"%")
+            )
+            return User.query.filter(filter_user).all()
         return User.query.all()
 
-    def resolve_all_books(self, info, **kwargs):
+    def resolve_all_books(self, info, search=None, **kwargs):
+        if search:
+            # Search both title and description
+            filter_book = (
+                Book.title.like('%'+search+'%') |
+                Book.description.like('%'+search+'%') 
+            )
+            return Book.query.filter(filter_book).all()
         return Book.query.all()
     # all_books = SQLAlchemyConnectionField(BookObject.connection)
     # all_users = SQLAlchemyConnectionField(UserObject.connection)
